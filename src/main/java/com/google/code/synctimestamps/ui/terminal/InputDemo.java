6a3f9b4530e04a9763ea69cfa5fff2d16bf15113
Andrey ``Bass'' Shcheglov <unix-junkie@users.noreply.github.com>
2012-06-27 12:35:04 +0000

    MAJOR: preliminary support for Windows added.

--- src/main/java/com/google/code/synctimestamps/ui/terminal/InputDemo.java
+++ src/main/java/com/google/code/synctimestamps/ui/terminal/InputDemo.java
@@ -16,6 +16,12 @@ import com.google.code.synctimestamps.ui.terminal.handlers.ExitHandler;
 import com.google.code.synctimestamps.ui.terminal.handlers.TerminalSizeHandler;
 
 /**
+ * This code has been designed for JNLP/Java Web Start use,
+ * as it launches external terminal emulators.
+ *
+ * Launching the application from command line should be done
+ * in a different way.
+ *
  * @author Andrew ``Bass'' Shcheglov (andrewbass@gmail.com)
  * @author $Author$
  * @version $Revision$, $Date$
@@ -57,61 +63,101 @@ public abstract class InputDemo {
 			return;
 		}
 
-		if (args.length == 1) {
+		if (!getProperty("os.name").startsWith("Windows")) {
 			/*
-			 * TTY device specified.
+			 * Most probably, POSIX.
 			 */
-			final String ttyName = args[0];
-			final Terminal term = new Terminal(ttyName, getenv("TERM"), new ExitHandler(new TerminalSizeHandler(new Echo())));
-			term.start();
+			if (args.length == 1) {
+				/*
+				 * TTY device specified.
+				 */
+				final String ttyName = args[0];
+				final Terminal term = new Terminal(ttyName, getenv("TERM"), new ExitHandler(new TerminalSizeHandler(new Echo())));
+				term.start();
+			} else {
+				final String javaCommandLine = System.getProperty("java.home") + File.separatorChar + "bin" + File.separatorChar + "java -classpath \"" + System.getProperty("java.class.path") + "\" " + InputDemo.class.getName();
+
+				final File shellScript = File.createTempFile("synctimestamps-" + getProperty("user.name") + '-', ".sh");
+				final PrintWriter out = new PrintWriter(shellScript);
+				final String shell = getenv("SHELL");
+				out.println("#!" + (shell != null && shell.length() != 0 ? shell : "/bin/sh"));
+				/*
+				 * In Cygwin, tty returns /dev/cons0, /dev/cons1,..
+				 * those values being invalid Windows filenames.
+				 */
+				out.println("tty=`tty 2>/dev/null`");
+				out.println("returnValue=$?");
+				out.println("if [ ${returnValue} -ne 0 ]");
+				out.println("then");
+				out.println("\texit ${returnValue}");
+				out.println("else");
+				/*
+				 * man stty
+				 */
+				final StringBuilder sttyCommand = new StringBuilder();
+				sttyCommand.append("\t");
+				sttyCommand.append("stty");
+				sttyCommand.append(" -icanon min 1 time 0"); // Allow individual characters to be read
+				sttyCommand.append(" -echo"); // Don't echo what is being read
+				sttyCommand.append(" -icrnl"); // ^M and <Enter> generate ^M, not ^J
+				sttyCommand.append(" intr undef"); // Allow ^C to be received
+				sttyCommand.append(" flush undef"); // Allow ^O to be received
+				sttyCommand.append(" start undef"); // Allow ^Q to be received
+				sttyCommand.append(" stop undef"); // Allow ^S to be received
+				sttyCommand.append(" lnext undef"); // Allow ^V to be received
+				sttyCommand.append(" dsusp undef"); // Allow ^Y to be received
+				sttyCommand.append(" susp undef"); // Allow ^Z to be received
+				sttyCommand.append(" quit undef"); // Allow ^\ to be received (otherwise JVM prints the full stack trace)
+				out.println(sttyCommand);
+				out.println("\t#stty -a");
+				out.println('\t' + javaCommandLine + " ${tty}");
+				out.println("\treturnValue=$?");
+				out.println("\tstty sane 2>/dev/null");
+				out.println("\t#echo \"Exiting with code ${returnValue}...\"");
+				out.println("\t#read dummy");
+				out.println("\texit ${returnValue}");
+				out.println("fi");
+				out.flush();
+				out.close();
+				shellScript.setExecutable(true);
+				shellScript.deleteOnExit();
+
+				final Process terminalProcess = newTerminalProcess("SyncTimeStamps", "SyncTimeStamps", shellScript.getPath());
+				if (terminalProcess == null) {
+					System.out.println("Failed to find a suitable terminal emulator in PATH.");
+					return;
+				}
+				final int returnValue = terminalProcess.waitFor();
+				if (returnValue != 0) {
+					System.out.println("Child process exited with code " + returnValue);
+				}
+				System.exit(returnValue);
+			}
 		} else {
-			final String javaCommandLine = System.getProperty("java.home") + File.separatorChar + "bin" + File.separatorChar + "java -classpath \"" + System.getProperty("java.class.path") + "\" " + InputDemo.class.getName();
-
-			final File shellScript = File.createTempFile("synctimestamps-" + getProperty("user.name") + '-', ".sh");
-			final PrintWriter out = new PrintWriter(shellScript);
-			out.println("#!" + getenv("SHELL"));
-			out.println("tty=`tty 2>/dev/null`");
-			out.println("returnValue=$?");
-			out.println("if [ ${returnValue} -ne 0 ]");
-			out.println("then");
-			out.println("\texit ${returnValue}");
-			out.println("else");
 			/*
-			 * man stty
+			 * Windows
 			 */
-			final StringBuilder sttyCommand = new StringBuilder();
-			sttyCommand.append("\t");
-			sttyCommand.append("stty");
-			sttyCommand.append(" -icanon min 1 time 0"); // Allow individual characters to be read
-			sttyCommand.append(" -echo"); // Don't echo what is being read
-			sttyCommand.append(" -icrnl"); // ^M and <Enter> generate ^M, not ^J
-			sttyCommand.append(" intr undef"); // Allow ^C to be received
-			sttyCommand.append(" flush undef"); // Allow ^O to be received
-			sttyCommand.append(" start undef"); // Allow ^Q to be received
-			sttyCommand.append(" stop undef"); // Allow ^S to be received
-			sttyCommand.append(" lnext undef"); // Allow ^V to be received
-			sttyCommand.append(" dsusp undef"); // Allow ^Y to be received
-			sttyCommand.append(" susp undef"); // Allow ^Z to be received
-			sttyCommand.append(" quit undef"); // Allow ^\ to be received (otherwise JVM prints the full stack trace)
-			out.println(sttyCommand);
-			out.println("\t#stty -a");
-			out.println('\t' + javaCommandLine + " ${tty}");
-			out.println("\treturnValue=$?");
-			out.println("\tstty sane 2>/dev/null");
-			out.println("\t#echo \"Exiting with code ${returnValue}...\"");
-			out.println("\t#read dummy");
-			out.println("\texit ${returnValue}");
-			out.println("fi");
-			out.flush();
-			out.close();
-			shellScript.setExecutable(true);
-			shellScript.deleteOnExit();
+			final String winDir = getenv("WINDIR");
+			final String cmdPath = (winDir == null || winDir.length() == 0 ? getenv("SYSTEMROOT") : winDir) + File.separatorChar + "system32" + File.separatorChar + "cmd.exe";
+			final String telnetPath = (winDir == null || winDir.length() == 0 ? getenv("SYSTEMROOT") : winDir) + File.separatorChar + "system32" + File.separatorChar + "telnet.exe";
+			final String telnetCommandLine[];
 
-			final Process terminalProcess = newTerminalProcess("SyncTimeStamps", "SyncTimeStamps", shellScript.getPath());
+			/**
+			 * @todo Instead of launching telnet.exe directly, create a batch file and modify window title.
+			 */
+			final boolean modifyDefaultColors = true;
+			if (modifyDefaultColors) {
+				telnetCommandLine = new String[]{cmdPath, "/C", "start", cmdPath, "/T:8A", "/K", telnetPath, "-t", "ansi"};
+			} else {
+				telnetCommandLine = new String[]{cmdPath, "/C", "start", telnetPath, "-t", "ansi"};
+			}
+
+			final Process terminalProcess = getRuntime().exec(telnetCommandLine);
 			if (terminalProcess == null) {
 				System.out.println("Failed to find a suitable terminal emulator in PATH.");
 				return;
 			}
+
 			final int returnValue = terminalProcess.waitFor();
 			if (returnValue != 0) {
 				System.out.println("Child process exited with code " + returnValue);
