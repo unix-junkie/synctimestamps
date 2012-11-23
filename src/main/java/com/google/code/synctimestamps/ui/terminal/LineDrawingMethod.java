1a2afe87ef2542de75f84b9ad0d02db65b54d8ec
Andrey ``Bass'' Shcheglov <unix-junkie@users.noreply.github.com>
2012-11-23 12:51:18 +0000

    ADD: initial ci

---
+++ src/main/java/com/google/code/synctimestamps/ui/terminal/LineDrawingMethod.java
@@ -0,0 +1,16 @@
+/*-
+ * $Id$
+ */
+package com.google.code.synctimestamps.ui.terminal;
+
+/**
+ * @author Andrew ``Bass'' Shcheglov (andrewbass@gmail.com)
+ * @author $Author$
+ * @version $Revision$, $Date$
+ */
+public enum LineDrawingMethod {
+	UNICODE,
+	VT100,
+	SUN_COLOR,
+	ASCII,
+}
