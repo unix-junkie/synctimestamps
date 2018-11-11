2332863af810d87b30a3a2f1c0a9a793dd8af27c
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2018-11-11 22:57:10 +0300

    Add unit tests

--- src/main/java/com/google/code/synctimestamps/DateTimeProvider.java
+++ src/main/java/com/google/code/synctimestamps/DateTimeProvider.java
@@ -4,6 +4,7 @@
 package com.google.code.synctimestamps;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.util.Date;
 
 import javax.annotation.CheckForNull;
@@ -20,14 +21,39 @@ public interface DateTimeProvider {
 	 *
 	 * @param file
 	 * @return the timestamp of a file, or {@code null}.
+	 * @see #getDateTime(Path)
 	 */
 	@Nullable
 	@CheckForNull
 	Date getDateTime(final File file);
 
 	/**
+	 * <p>Returns the timestamp of a file on the file system, or {@code
+	 * null} if this provider can't determine the timestamp, or the
+	 * timestamp is before Jan 1st, 1970.</p>
+	 *
+	 * @param file
+	 * @return the timestamp of a file, or {@code null}.
+	 * @see #getDateTime(File)
+	 */
+	@Nullable
+	default Date getDateTime(final Path file) {
+		return this.getDateTime(file.toFile());
+	}
+
+	/**
 	 * @param file
 	 * @param parentDateTime
+	 * @see #updateDateTime(Path, Date)
 	 */
 	void updateDateTime(final File file, @Nullable final Date parentDateTime);
+
+	/**
+	 * @param file
+	 * @param parentDateTime
+	 * @see #updateDateTime(File, Date)
+	 */
+	default void updateDateTime(final Path file, @Nullable final Date parentDateTime) {
+		this.updateDateTime(file.toFile(), parentDateTime);
+	}
 }
---
+++ src/test/java/com/google/code/synctimestamps/FileNameBasedTest.java
@@ -0,0 +1,121 @@
+/*-
+ * $Id$
+ */
+package com.google.code.synctimestamps;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+import static java.nio.file.Files.createFile;
+import static java.nio.file.Files.createTempDirectory;
+import static java.nio.file.Files.delete;
+import static java.nio.file.Files.getLastModifiedTime;
+import static java.nio.file.Files.walkFileTree;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Andrey ``Bass'' Shcheglov &lt;mailto:andrewbass@gmail.com&gt;
+ */
+public class FileNameBasedTest {
+	private static final ThreadLocal<Path> WORKING_DIR = new ThreadLocal<>();
+
+	@SuppressWarnings("static-method")
+	@Before
+	public void setUp() throws IOException {
+		final Path workingDir = createTempDirectory("synctimestamps-");
+		WORKING_DIR.set(workingDir);
+	}
+
+	@SuppressWarnings("static-method")
+	@After
+	public void tearDown() throws IOException {
+		final Path workingDir = WORKING_DIR.get();
+		walkFileTree(workingDir, new SimpleFileVisitor<Path>() {
+				@Override
+				public FileVisitResult visitFile(final Path file,
+						final BasicFileAttributes attrs)
+				throws IOException {
+					delete(file);
+					return CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult visitFileFailed(final Path file,
+						final IOException ioe)
+				throws IOException {
+					ioe.printStackTrace();
+					return CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult postVisitDirectory(final Path dir,
+						final IOException ioe)
+				throws IOException {
+					if (ioe != null) {
+						ioe.printStackTrace();
+					}
+
+					delete(dir);
+					return CONTINUE;
+				}
+		});
+	}
+
+	@SuppressWarnings("static-method")
+	@Test
+	public void testSingleWithDefaultPattern() throws IOException, ParseException {
+		final Path workingDir = WORKING_DIR.get();
+
+		final FileNameBased dateTimeProvider = new FileNameBased();
+		dateTimeProvider.setFileNamePattern("(\\d{4}\\-\\d{2}\\-\\d{2}_\\d{2}\\-\\d{2}\\-\\d{2})_(.*)");
+		dateTimeProvider.setDateFormatPattern("yyyy-MM-dd_HH-mm-ss");
+		dateTimeProvider.setSeparator('_');
+
+		final Date referenceDate = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").parse("1971-04-12_09-07-00");
+
+		final Path file0 = createFile(workingDir.resolve("1971-04-12_09-07-00_name.jpg"));
+		final Date dateTime0 = dateTimeProvider.getDateTime(file0);
+		assertThat(dateTime0).isNotNull().isEqualTo(referenceDate);
+		assertThat(dateTime0.getTime()).isNotEqualTo(getLastModifiedTime(file0).toMillis());
+
+		final Path file1 = createFile(workingDir.resolve("1971-04-12_09-07-00_name.mp4"));
+		final Date dateTime1 = dateTimeProvider.getDateTime(file1);
+		assertThat(dateTime1).isNotNull().isEqualTo(referenceDate);
+		assertThat(dateTime1.getTime()).isNotEqualTo(getLastModifiedTime(file1).toMillis());
+	}
+
+	@SuppressWarnings("static-method")
+	@Test
+	public void testNestedWithDefaultPattern() throws IOException, ParseException {
+		final Path workingDir = WORKING_DIR.get();
+
+		final FileNameBased dateTimeProvider = new FileNameBased();
+		dateTimeProvider.setFileNamePattern("(\\d{4}\\-\\d{2}\\-\\d{2}_\\d{2}\\-\\d{2}\\-\\d{2})_(.*)");
+		dateTimeProvider.setDateFormatPattern("yyyy-MM-dd_HH-mm-ss");
+		dateTimeProvider.setSeparator('_');
+		dateTimeProvider.setNext(new MtimeBased());
+
+		final Date referenceDate = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").parse("1971-04-12_09-07-00");
+
+		final Path file0 = createFile(workingDir.resolve("1971-04-12_09-07-00_name.jpg"));
+
+		final Path file1 = createFile(workingDir.resolve("1971-04-12_09-07-00_name.mp4"));
+
+		dateTimeProvider.updateDateTime(file0, referenceDate);
+		dateTimeProvider.updateDateTime(file1, referenceDate);
+
+		assertThat(getLastModifiedTime(file0).toMillis()).isEqualTo(referenceDate.getTime());
+		assertThat(getLastModifiedTime(file1).toMillis()).isEqualTo(referenceDate.getTime());
+	}
+}
